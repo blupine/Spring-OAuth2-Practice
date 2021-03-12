43a23743c1ea4c40746888cd81c3a59365eb5e80
blupine <skwint11@gmail.com>
2021-03-12 20:15:17 +0900

    Add basic spring security config

---
+++ src/main/java/com/bbolab/godingeta/Config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.bbolab.godingeta.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .authorizeRequests()
+                .antMatchers("/api/login", "/api/signup").permitAll()
+                .anyRequest().authenticated();
+    }
+}
